2d99b8bc461127900dfbbe1e5f2fa1bb5dacc36a
yepengding <yepengd@acm.org>
2025-01-04 11:40:29 +0800

    Add strategy execution lock.

--- src/executor/executor.service.ts
+++ src/executor/executor.service.ts
@@ -15,7 +15,11 @@ import { StrategyClass } from '../strategy/strategy.types';
 export class ExecutorService {
   private readonly logger = new Logger(ExecutorService.name);
 
+  // Execution counter
   private counter: number;
+
+  // Execution lock to ensure singleton
+  private isLocked: Map<string, boolean>;
 
   constructor(
     private schedulerRegistry: SchedulerRegistry,
@@ -23,6 +27,7 @@ export class ExecutorService {
     private stateRepository: Repository<StrategyState>,
   ) {
     this.counter = 0;
+    this.isLocked = new Map<string, boolean>();
   }
 
   async run(
@@ -41,16 +46,24 @@ export class ExecutorService {
       this.logger.error(e.toString());
       return false;
     }
+    // Initialize strategy execution lock
+    this.isLocked.set(strategyId, false);
 
     // Schedule strategy execution
     this.counter = this.counter > 58 ? 0 : this.counter + 1;
     const job = new CronJob(`${this.counter}-59/5 * * * * *`, async () => {
       try {
-        await strategy.next();
+        if (!this.isLocked.get(strategyId)) {
+          this.isLocked.set(strategyId, true);
+          await strategy.next();
+        } else {
+          this.logger.warn(`${strategyId} is locked`);
+        }
       } catch (e) {
         this.logger.error(e);
         this.logger.error(`${strategyId} crashed`);
-        return;
+      } finally {
+        this.isLocked.set(strategyId, false);
       }
     });
     this.schedulerRegistry.addCronJob(strategyId, job);
@@ -70,9 +83,11 @@ export class ExecutorService {
     } else if (runningJob.running) {
       runningJob.stop();
       this.schedulerRegistry.deleteCronJob(strategyId);
+      this.isLocked.delete(strategyId);
       return true;
     } else {
       this.schedulerRegistry.deleteCronJob(strategyId);
+      this.isLocked.delete(strategyId);
       return true;
     }
   }
