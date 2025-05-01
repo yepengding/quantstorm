903b07e4b7683c4fb181ce81539213c7a3ff1e29
yepengding <yepengd@acm.org>
2025-05-01 15:26:36 +0900

    Remove redundant scripts.

--- rebuild_quanstorm.sh
+++
@@ -1,5 +0,0 @@
-# Building command for Quantstorm
-docker stop quantstorm
-docker rm quantstorm
-docker rmi quantstorm
-docker build -t quantstorm .
--- run_quanstorm.sh
+++
@@ -1,1 +0,0 @@
-docker run --name quantstorm -p 80:3000 -v /home/ubuntu/logs:/logs -v /home/ubuntu/db:/db -d quantstorm
